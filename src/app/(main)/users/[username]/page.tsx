f42493242b7e298e4a88cc84cf96433e49c114be
JoseValperga <jose.valperga@gmail.com>
2024-04-24 16:29:07 -0300

    Final modulo 1

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Message from "@/components/messages/Message";
 import UserTabs from "@/components/users/UserTabs";
+import Image from "next/image";
+import profilePic from "../../../../../public/anakin.jpg";
 
 const UserPage = ({ params }: { params: { username: string } }) => {
   const user = {
@@ -29,8 +31,18 @@ const UserPage = ({ params }: { params: { username: string } }) => {
   return (
     <main className="flex flex-col bg-gray-100 p-8">
       <section className="flex flex-col mb-8">
-        <div className="rounded-full p-6 bg-gray-300 w-20 text-center">
-          <span className="font-semibold text-lg">AS</span>
+        <div className="rounded-full text-center mb-4 block relative w-20 h-20">
+          <Image
+            className=" rounded-full"
+            src={profilePic}
+            alt="Picture of the author"
+            fill
+            priority
+            // width={500} automatically provided
+            // height={500} automatically provided
+            // blurDataURL="data:..." automatically provided
+            placeholder="blur" // Optional blur-up while loading
+          />
         </div>
 
         <h2 className="mb-1">{user.name}</h2>
--- src/components/messages/Message.tsx
+++ src/components/messages/Message.tsx
@@ -1,5 +1,6 @@
 import { MessageType } from "@/types/types.message";
 import Link from "next/link";
+import Image from "next/image";
 
 type MessageProps = {
   message: MessageType;
@@ -8,7 +9,7 @@ type MessageProps = {
 const Message = ({ message }: MessageProps) => {
   return (
     <div className="flex">
-      <div className="rounded-full p-5 bg-gray-300 w-16 text-center">
+      <div className="rounded-full p-5 bg-gray-300 w-16 text-center mb-4">
         <span className="font-semibold text-sm">AS</span>
       </div>
       <div className="flex flex-col ml-4 mt-2">
@@ -19,6 +20,12 @@ const Message = ({ message }: MessageProps) => {
           </div>
         </div>
         <p>{message.message}</p>
+        <Image
+          src="https://viajes.nationalgeographic.com.es/medio/2022/03/25/la-digue-seychelles_bea7ddd0_1255x836.jpg"
+          alt="Picture of the author"
+          width={300}
+          height={300}
+        />
       </div>
     </div>
   );
